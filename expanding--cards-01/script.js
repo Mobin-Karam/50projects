bd6a7295f5ba4bb70118d8102ad775e46a5fdfcc
Mobin-Karam <mohammadmobinkaram@gmail.com>
2024-02-03 10:35:42 +0330

    complete project , add responsive

--- expanding--cards-01/script.js
+++ expanding--cards-01/script.js
@@ -14,11 +14,24 @@ for (let i = 0; i < description.length; i++) {
   descriptionArray.push(element);
 }
 
-imageArray.map((image, index) => {
-  image.addEventListener("click", () => {
-    descriptionArray.forEach((des) => des.classList.remove("visibile"));
-    imageArray.forEach((img) => img.classList.remove("active"));
-    descriptionArray[index].classList.add("visibile");
-    image.classList.add("active");
+if (window.innerWidth > 900) {
+  imageArray.map((image, index) => {
+    image.addEventListener("click", () => {
+      descriptionArray.forEach((des) => des.classList.remove("visibile"));
+      imageArray.forEach((img) => img.classList.remove("active"));
+      descriptionArray[index].classList.add("visibile");
+      image.classList.add("active");
+    });
   });
-});
+  console.log("900");
+}else  (window.innerWidth <= 900) {
+  imageArray.map((image, index) => {
+    image.addEventListener("click", () => {
+      descriptionArray.forEach((des) => des.classList.remove("visibile"));
+      imageArray.forEach((img) => img.classList.remove("active2"));
+      descriptionArray[index].classList.add("visibile");
+      image.classList.add("active2");
+    });
+  });
+  console.log("900px");
+}
