318350b9e6beda5544d90b0f5ab73e20d6b40c2d
Mobin-Karam <mohammadmobinkaram@gmail.com>
2024-02-03 10:51:36 +0330

    fix bug 02

--- expanding--cards-01/script.js
+++ expanding--cards-01/script.js
@@ -14,26 +14,12 @@ for (let i = 0; i < description.length; i++) {
   descriptionArray.push(element);
 }
 
-if (window.innerWidth > 900) {
-  imageArray.map((image, index) => {
-    image.addEventListener("click", () => {
-      imageArray.forEach((img) => img.classList.remove("active2"));
-      descriptionArray.forEach((des) => des.classList.remove("visibile"));
-      imageArray.forEach((img) => img.classList.remove("active"));
-      descriptionArray[index].classList.add("visibile");
-      image.classList.add("active");
-    });
+imageArray.map((image, index) => {
+  image.addEventListener("click", () => {
+    descriptionArray.forEach((des) => des.classList.remove("visibile"));
+    imageArray.forEach((img) => img.classList.remove("active"));
+    descriptionArray[index].classList.add("visibile");
+    image.classList.add("active");
   });
-  console.log("900");
-} else {
-  imageArray.map((image, index) => {
-    image.addEventListener("click", () => {
-      imageArray.forEach((img) => img.classList.remove("active"));
-      descriptionArray.forEach((des) => des.classList.remove("visibile"));
-      imageArray.forEach((img) => img.classList.remove("active2"));
-      descriptionArray[index].classList.add("visibile");
-      image.classList.add("active2");
-    });
-  });
-  console.log("900px");
-}
+});
+console.log("900");
